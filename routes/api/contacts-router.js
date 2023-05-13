0625cc2979b293dac285d3aa0463bbffe254748a
Andrew <shatokhin.a.p@gmail.com>
2023-05-13 21:44:45 +0300

    Add decorators and controllers

---
+++ routes/api/contacts-router.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const contactsController = require("../../controllers/contacts-controllers");
+
+const schemas = require("../../schemas/contacts-schemas");
+const { validateBody } = require("../../decorators");
+
+const router = express.Router();
+
+
+
+router.get("/", contactsController.listContacts);
+
+router.get("/:id", contactsController.getContactById);
+
+router.post("/", validateBody(schemas.contactAddSchema), contactsController.addContact);
+
+router.put(
+  "/:id",
+  validateBody(schemas.contactAddSchema),
+  contactsController.updateContact
+);
+
+router.delete("/:id", contactsController.removeContact);
+
+module.exports = router
