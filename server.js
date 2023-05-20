0a980933d500aea033b7a99c360b6674e68acab5
Andrew <shatokhin.a.p@gmail.com>
2023-05-20 23:02:32 +0300

    add connect to MongoDB

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Andrew:SDBrYQpePbTFjK1R@cluster0.hycmx6x.mongodb.net/db_contacts?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST).then(() => {
+  app.listen(3000, () => {
+    console.log("Database connection successful");
+  });
+  
+}).catch(error => {
+  console.log(error.message);
+  process.exit(1);
+});
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
+
+// Andrew
+// SDBrYQpePbTFjK1R
