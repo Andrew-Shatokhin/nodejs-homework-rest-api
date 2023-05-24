6dea5e82c8ef504a1a9cc8398a4b9b16a663f79d
Andrew <shatokhin.a.p@gmail.com>
2023-05-24 23:29:57 +0300

    add user

---
+++ routes/api/auth.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const ctrl = require('../../controllers/auth');
+
+const { validateBody } = require("../../decorators");
+
+const { schemas } = require('../../models/user');
+
+const router = express.Router();
+
+
+router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
+
+
+module.exports = router;
+
