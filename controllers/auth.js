483fbc7315995714075c4ce3891d3c4cbf3d28f0
Andrew <shatokhin.a.p@gmail.com>
2023-05-26 20:36:21 +0300

    add jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,13 @@
 const bcrypt = require("bcrypt");
 
+const jwt = require("jsonwebtoken")
+
 const { User } = require("../models/user");
+
 const { HttpError } = require("../helpers");
+
+const { SECRET_KEY } = process.env;
+
 const { ctrlWrapper } = require("../decorators");
 
 const register = async (req, res) => {
@@ -34,7 +40,12 @@ if (!passwordCompare) {
   throw HttpError(401, "Email or password is wrong");
   }
   
-  const token = "hkjfh.skjfhsj.fhsjd";
+  const payload = {
+  id: user._id,
+}
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  
   res.json({
     token,
   })
