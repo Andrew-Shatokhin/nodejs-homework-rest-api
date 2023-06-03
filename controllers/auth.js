023242e3ae2f653f1c019ac37ae8ebd7b083c42b
Andrew <shatokhin.a.p@gmail.com>
2023-06-03 20:06:43 +0300

    add jimp

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
+const jimp = require("jimp");
 const fs = require("fs/promises");
 
 const { User } = require("../models/user");
@@ -87,8 +88,12 @@ const updateAvatar = async (req, res) => {
   const { _id } = req.user;
 
   const { path: tempUpload, originalname } = req.file;
+const img = await jimp.read(tempUpload);
+    await img.cover(250, 250).writeAsync(tempUpload);
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
+
+  
   await fs.rename(tempUpload, resultUpload);
   const avatarURL = path.join("avatars", filename);
   await User.findByIdAndUpdate(_id, { avatarURL });
